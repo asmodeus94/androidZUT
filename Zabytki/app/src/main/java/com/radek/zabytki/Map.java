d542115edb6c68edab677556fb4abec35379ec5c
Radoslaw Adamowicz <r.adamowicz4@wp.pl>
2017-06-05 21:34:51 +0200

    Dodano obsluge uprawnien zwiazanych z usluga lokalizacji.

--- Zabytki/app/src/main/java/com/radek/zabytki/Map.java
+++ Zabytki/app/src/main/java/com/radek/zabytki/Map.java
@@ -1,18 +1,33 @@
 package com.radek.zabytki;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Map extends FragmentActivity implements OnMapReadyCallback {
+import java.util.List;
+import java.util.Locale;
 
+public class Map extends FragmentActivity
+        implements OnMapReadyCallback, LocationAssistant.Listener {
+
+    private LocationAssistant assistant;
     private GoogleMap mMap;
+    private Marker myPositionMarker;
     MySQLite mySQLite;
     Monument monument;
     LatLng nowy;
@@ -25,9 +40,23 @@ public class Map extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        mySQLite=new MySQLite(this);
+        mySQLite = new MySQLite(this);
+        assistant = new LocationAssistant(this, this, LocationAssistant.Accuracy.HIGH, 5000, false);
+        assistant.setVerbose(true);
     }
 
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        assistant.start();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        assistant.stop();
+    }
 
     /**
      * Manipulates the map once available.
@@ -43,11 +72,10 @@ public class Map extends FragmentActivity implements OnMapReadyCallback {
         mMap = googleMap;
 
 
-        for (int i = 1; i <= mySQLite.lista().getCount(); i++){
+        for (int i = 1; i <= mySQLite.lista().getCount(); i++) {
             monument = mySQLite.pobierz(i);
 
             nowy = new LatLng(monument.getLongitude(), monument.getLatitude());
-
 
 
             mMap.addMarker(new MarkerOptions()
@@ -58,10 +86,115 @@ public class Map extends FragmentActivity implements OnMapReadyCallback {
 
             //mMap.moveCamera(CameraUpdateFactory.newLatLng(nowy));
 
-        //LatLng sydney = new LatLng(-34, 151);
-        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            //LatLng sydney = new LatLng(-34, 151);
+            //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+            //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         }
+
+    }
+
+    @Override
+    public void onNeedLocationPermission() {
+        assistant.requestAndPossiblyExplainLocationPermission();
+    }
+
+    @Override
+    public void onExplainLocationPermission() {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.permissionExplanation)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        assistant.requestLocationPermission();
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
+    }
+
+    @Override
+    public void onLocationPermissionPermanentlyDeclined(View.OnClickListener fromView, DialogInterface.OnClickListener fromDialog) {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.permissionPermanentlyDeclined)
+                .setPositiveButton(R.string.ok, fromDialog)
+                .show();
+    }
+
+    @Override
+    public void onNeedLocationSettingsChange() {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.switchOnLocationShort)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        assistant.changeLocationSettings();
+                    }
+                })
+                .show();
+    }
+
+    @Override
+    public void onFallBackToSystemSettings(View.OnClickListener fromView, DialogInterface.OnClickListener fromDialog) {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.switchOnLocationLong)
+                .setPositiveButton(R.string.ok, fromDialog)
+                .show();
+    }
+
+    public String getCompleteAddressString(double LATITUDE, double LONGITUDE) {
+        String strAdd = "";
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(LATITUDE, LONGITUDE, 1);
+            if (addresses != null) {
+                Address returnedAddress = addresses.get(0);
+                StringBuilder strReturnedAddress = new StringBuilder("");
+
+                for (int i = 0; i < returnedAddress.getMaxAddressLineIndex(); i++) {
+                    strReturnedAddress.append(returnedAddress.getAddressLine(i)).append("\n");
+                }
+                strAdd = strReturnedAddress.toString();
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return strAdd;
+    }
+
+    @Override
+    public void onNewLocationAvailable(Location location) {
+        if (assistant.getBestLocation() != null) {
+            // test {
+            String geolocation = getCompleteAddressString(location.getLatitude(), location.getLongitude());
+            LatLng myPosition = new LatLng(location.getLatitude(), location.getLongitude());
+            MarkerOptions markerOptions = new MarkerOptions().position(myPosition).title(geolocation).icon(BitmapDescriptorFactory
+                    .defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+            if (myPositionMarker == null) {
+                myPositionMarker = mMap.addMarker(markerOptions);
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myPosition, 16));
+            } else {
+                myPositionMarker.setPosition(myPosition);
+            }
+        }
+    }
+
+    @Override
+    public void onMockLocationsDetected(View.OnClickListener fromView, DialogInterface.OnClickListener fromDialog) {
+
+    }
+
+    @Override
+    public void onError(LocationAssistant.ErrorType type, String message) {
 
     }
 }
